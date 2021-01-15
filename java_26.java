ae8878d9e679182bec94ae18af32aa4978a72300
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-16 02:05:44 +0900

    java 200제 26번. 상수 사용과 Math함수 처음 사용.

---
+++ java_26.java
@@ -0,0 +1,16 @@
+package study;
+
+public class java_26 {
+
+	public static final int PHYSICAL =23;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  System.out.println(PHYSICAL);
+	  int index=PHYSICAL;
+	  double vals=2*Math.PI/index;
+	  System.out.println(vals+"¶óµð¾È");
+		
+
+	}
+
+}
