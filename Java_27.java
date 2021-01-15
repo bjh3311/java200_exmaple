aea46baf5f5864793cfbf716697a9648fefd32ce
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-16 02:15:00 +0900

    java 200제 27번. Math.toDegrees. 라디안을 각도로 바꿔주는 함수 사용해봄

---
+++ Java_27.java
@@ -0,0 +1,15 @@
+package java_27;
+
+public class Java_27 {
+
+	public static final int PHYSICAL=23;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int index=PHYSICAL;
+		int days=1200;
+		double vals=(days%index)*2*Math.PI/index;
+		System.out.println(Math.toDegrees(vals)+"µµ");
+
+	}
+
+}
