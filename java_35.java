6428985a0439a81781978a72b004859028252797
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-16 02:22:19 +0900

    java 200제 35번. 삼항 연산자 사용해보았다.

---
+++ java_35.java
@@ -0,0 +1,11 @@
+package java_35;
+
+public class java_35 {
+	public static void main(String[] args)
+	{
+		int temp=99;
+		temp=(temp%2==1)?temp*3+1:temp/2;
+		System.out.printf("°è»ê ÈÄ=%d\n",temp);
+	}
+
+}
